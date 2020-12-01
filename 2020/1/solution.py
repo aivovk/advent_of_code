9b04047bdd7e85903fc739265f5d1c3c70eab39d
Andrei Vovk <andrei.vovk@mail.utoronto.ca>
2020-12-01 07:29:29 -0500

    - non brute force solution to part 2

--- 2020/1/solution.py
+++ 2020/1/solution.py
@@ -9,10 +9,9 @@ def partOne():
             differences.add(2020 - n)
 
             
-def partTwo():
+def partTwoBruteForce():
     with open("input.txt") as f:
         nums = [int(line) for line in f]
-
     nums.sort()
 
     # brute force for now
@@ -21,6 +20,27 @@ def partTwo():
             for i3 in range( i2 + 1, len(nums)):
                 if nums[i1] + nums[i2] + nums[i3] == 2020:
                     return nums[i1]*nums[i2]*nums[i3]
+def partTwo():
+    with open("input.txt") as f:
+        nums = [int(line) for line in f]
+    nums.sort()
+    setOfNums = set(nums)
 
-print(partOne())
-print(partTwo())
+    for i1 in range(len(nums)):
+        for i2 in range(i1+1,len(nums)):
+            n1 = nums[i1]
+            n2 = nums[i2]            
+            n3 = 2020 - nums[i1] - nums[i2]
+            if n3 in setOfNums and n3 != n1 and n3 != n2:
+                return n1 * n2 * n3
+
+# all numbers are different
+def checkFile():
+    with open("input.txt") as f:
+        nums = [int(line) for line in f]
+    assert(len(nums) == len(set(nums)))
+
+if __name__=="__main__":
+    checkFile()
+    print(partOne())
+    print(partTwo())
