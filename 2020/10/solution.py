340a75e6660860e90744f0eae4e4b94a378958a4
Andrei Vovk <andrei.vovk@mail.utoronto.ca>
2020-12-11 16:22:29 -0500

    Solve Day 11, 2020 (slow?), cleaner Day 10

--- 2020/10/solution.py
+++ 2020/10/solution.py
@@ -3,29 +3,22 @@ def load():
     return sorted(data)
 
 def partTwo(data):
-    """
-  
-    """
-    data = [0] + data # add starting location
+    data = [0,0,0] + data # add buffer (0,1) and starting location (2)
     count = [0] * len(data) # number of ways of reaching that adapter
-    count[0] = 1
-    for i in range(1, len(data)):
+    count[2] = 1
+    for i in range(3, len(data)):
         count[i] = count[i-1] # always reachable from previous
 
         # check if reachable from adapters 2 and 3 spots ago
-        r2 =  (i > 1) and (4 > (data[i] - data[i-2]))
-        r3 =  (i > 2) and (4 > (data[i] - data[i-3]))
-        if r2:
+        if data[i] - data[i-2] <= 3:
             count[i] += count[i-2]
-        if r3:
-            count[i] += count[i-3]
+            if data[i] - data[i-3] <= 3:
+                count[i] += count[i-3]
     return count[-1]
 
 def partOne(data):
-    """
-    
-    """
-    #diffs = data[1:] - data[:-1]
+    #diffs = [*map(int.__sub__, data[1:], data[:-1])]
+    #return (1+diffs.count(1))*(1+diffs.count(3))
     c1 = int(data[0] == 1)
     c3 = 1 + int(data[0] == 3)
     for i in range(1, len(data)):
