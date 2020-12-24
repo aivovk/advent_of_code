b99ab7d01f2b8bf126762d69cf8b8557e05cbbb3
Andrei Vovk <andrei.vovk@mail.utoronto.ca>
2020-12-24 09:11:10 +0000

    Solve Day 24,2020

--- 2020/20/solution.py
+++ 2020/20/solution.py
@@ -1,8 +1,8 @@
 import numpy as np
 import math
+import collections
 
 def partOne():
-    s = 0
     with open('input.txt', 'r') as f:
         lines = f.readlines()
         tile_strs = ''.join(lines).split('\n\n')
@@ -33,5 +33,119 @@ def edgesMatch(tileA, tileB):
     
     return False
 
+def readTiles(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        tile_strs = ''.join(lines).split('\n\n')
+        tiles = [np.reshape(np.array(list(''.join(tile_str.split('\n')[1:11]))), (10,10)) for tile_str in tile_strs]
+        tileIDs = [int(id_str[5:id_str.find(':')]) for id_str in tile_strs]
+    return tiles, tileIDs
+
+def findAdjacent(tiles):
+    adj_tiles = collections.defaultdict(set)
+    
+    for i in range(len(tiles)):
+        for j in range(i+1,len(tiles)):
+            if len(adj_tiles[i]) < 4 and len(adj_tiles[j]) < 4 and edgesMatch(tiles[i], tiles[j]):
+                adj_tiles[j].add(i)
+                adj_tiles[i].add(j)
+    return adj_tiles
+
+# find which edge of A matches B
+def match(tileA, tileB):
+    for i in range(4):
+        if (tileA[0] == tileB[0]).all()\
+           or (tileA[0] == tileB[0,::-1]).all():
+            return 0
+        if (tileA[-1] == tileB[-1]).all()\
+           or (tileA[-1] == tileB[-1,::-1]).all():
+            return 2
+        if (tileA[:,0] == tileB[:,0]).all()\
+           or (tileA[:,0] == tileB[::-1,0]).all():
+            return 1
+        if (tileA[:,-1] == tileB[:,-1]).all()\
+           or (tileA[:,-1] == tileB[::-1,-1]).all():
+            return 3
+        tileB = np.rot90(tileB)
+
+
 if __name__=="__main__":
-    print(partOne())
+    #print(partOne())
+    tiles, tileIDs = readTiles('input2.txt')
+    adj_tiles = findAdjacent(tiles)
+  
+    size = int(len(tiles)**0.5)
+    puzzle = [[0 for i in range(size)] for i in range(size)]
+
+    starting_corner = [k for k, v in adj_tiles.items() if len(v)==2][0]
+
+    puzzle[0][0] = starting_corner
+    
+    for row in range(size):
+        for col in range(size):
+            """
+            always going left to right and top to bottom
+            so we need to match with row -1 and col -1
+            """
+            if row + col > 0:
+                # to_match is the left and above pieces
+                to_match = []
+                if row > 0:
+                    to_match.append(puzzle[row-1][col])
+                if col > 0:
+                    to_match.append(puzzle[row][col-1])
+
+                # matches is the set of adjacent pieces shared by to_match
+                matches = set(adj_tiles[to_match[0]])
+                for m in to_match[1:]:
+                    matches &= adj_tiles[m]
+
+                # choosing the piece with the min adjacent tiles ensures we
+                # match walls instead of sides when we need to
+                fits = 5
+                best_piece = None
+                for m in matches:
+                    if len(adj_tiles[m]) < fits:
+                        fits = len(adj_tiles[m])
+                        best_piece = m
+                assert(best_piece is not None)
+                puzzle[row][col] = best_piece
+                
+                for m in to_match:
+                    adj_tiles[m].remove(puzzle[row][col])
+                    adj_tiles[puzzle[row][col]].remove(m)
+    for row in range(size):
+        for col in range(size):
+            print(tiles[puzzle[row][col]])
+    '''
+    top 0
+    left 1
+    bottom 2
+    right 3
+
+    get the matches
+
+    depending on that, we know whether to flip or not
+    '''
+    
+    for row in range(size - 1):
+        for col in range(size - 1):
+            current = puzzle[row][col]
+            right = match(tiles[current], tiles[puzzle[row+1][col]])
+            bottom = match(tiles[current], tiles[puzzle[row][col+1]])
+            while right%4 != 3:
+                tiles[current] = np.rot90(tiles[current])
+                right += 1
+                bottom += 1
+            #if bottom%4 != 2: # flip vertically
+            #    tiles[current] = np.flipud(tiles[current])
+
+    for rPuzzle in range(size):
+        for row in range(len(tiles[0])):
+            r = ''
+            for cPuzzle in range(size):
+                r+=' '
+                for col in range(len(tiles[0])):
+                    r += tiles[puzzle[rPuzzle][cPuzzle]][row,col]
+            print(r)
+        print('')
---
+++ 2020/24/solution.py
@@ -0,0 +1,62 @@
+import collections
+
+dirs = {'e' : (1,0),
+        'se' : (1,1),
+        'sw' : (0,1),
+        'w' : (-1,0),
+        'nw' : (-1,-1),
+        'ne' : (0,-1)}
+
+def partOne():
+    tiles = collections.defaultdict(bool)
+    with open('input.txt', 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            locx = 0
+            locy = 0
+            i = 0
+            while i < len(line):
+                if line[i] in dirs.keys():
+                    d = dirs[line[i]]
+                    i += 1
+                else:
+                    d = dirs[line[i:i+2]]
+                    i += 2
+                locx += d[0]
+                locy += d[1]
+            tiles[(locx,locy)] = not tiles[(locx, locy)]
+    return sum(1 for k,v in tiles.items() if v), tiles
+
+def countNeighbours(tiles, x, y):
+    n = 0
+    for k,v in dirs.items():
+        n += tiles[(x+v[0],y+v[1])]
+    return n
+
+def partTwo(tiles):
+    minx = min(x for x,y in tiles.keys()) - 1
+    maxx = max(x for x,y in tiles.keys()) + 1
+    miny = min(y for x,y in tiles.keys()) - 1
+    maxy = max(y for x,y in tiles.keys()) + 1
+
+    for step in range(100):
+        nextstep = collections.defaultdict(bool)
+        for x in range(minx, maxx+1):
+            for y in range(miny, maxy+1):
+                n = countNeighbours(tiles, x, y)
+                if tiles[(x,y)]: # black
+                    if n == 1 or n == 2:
+                        nextstep[(x,y)] = True
+                elif n == 2: # white
+                    nextstep[(x,y)] = True
+        tiles = nextstep
+        minx -= 1
+        miny -= 1
+        maxx += 1
+        maxy += 1
+    return sum(1 for k,v in tiles.items() if v)
+
+if __name__=="__main__":
+    day0, tiles = partOne()
+    print(day0)
+    print(partTwo(tiles))
